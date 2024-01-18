0ba274a53fe51d1bb24e1e616649f91cbb187150
Hulekal <sachin.hulekal@hp.com>
2024-01-18 15:33:02 +0530

    uplod to aws

--- src/testLiqui/testClass.java
+++ src/testLiqui/testClass.java
@@ -1,18 +1,53 @@
 package testLiqui;
 
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-// Handler value: example.HandlerInteger
-public class testClass implements RequestHandler<Integer, String>{
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.resource.FileSystemResourceAccessor;
 
-  @Override
+// Handler value: example.HandlerInteger
+public abstract class testClass implements RequestHandler<Integer, String>{
   /*
    * Takes in an InputRecord, which contains two integers and a String.
    * Logs the String, then returns the sum of the two Integers.
    */
-  public String handleRequest(Integer event, Context context)
-  {
-    return String.valueOf(event);
-  }
+   public String CHANGELOG_PATH = "C:/test/testLiqui/src/resource/changelog.sql";
+
+	public void handleRequest(String[] args) {
+		try {
+			Properties props = new Properties();
+			FileInputStream fis = new FileInputStream("C:/test/testLiqui/src/resource/db.properties");
+			props.load(fis);
+			fis.close();
+
+			String url = props.getProperty("url");
+			String username = props.getProperty("username");
+			String password = props.getProperty("password");
+
+			Connection con = DriverManager.getConnection(url, username, password);
+			System.out.println(con);
+
+	        Database database = DatabaseFactory.getInstance()
+	                .findCorrectDatabaseImplementation(new JdbcConnection(con));
+	        Liquibase liquibase = new Liquibase( 
+	        		"C:/Users/HulekalS03/Downloads/eclipse-java-2022-12-R-win32-x86_64/eclipse/com.test/src/main/resources/changelog.sql", 
+	        	    new FileSystemResourceAccessor(), 
+	        	    database 
+	        	);
+	        liquibase.update( "" );
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 }
