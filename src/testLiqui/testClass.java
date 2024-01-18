4eb9e99107320c25f5184281dbbdac3df35b32df
Hulekal <sachin.hulekal@hp.com>
2024-01-18 16:52:05 +0530

    uplod to aws

--- src/testLiqui/testClass.java
+++ src/testLiqui/testClass.java
@@ -16,21 +16,21 @@ import liquibase.database.jvm.JdbcConnection;
 import liquibase.resource.FileSystemResourceAccessor;
 
 // Handler value: example.HandlerInteger
-public class testClass implements RequestHandler<Map<String,String>, String>{
-  /*
-   * Takes in an InputRecord, which contains two integers and a String.
-   * Logs the String, then returns the sum of the two Integers.
-   */
-	private static final String CHANGELOG_PATH = "C:/test/testLiqui/src/resource/changelog.sql";
-	
+public class testClass implements RequestHandler<Map<String, String>, String> {
+	/*
+	 * Takes in an InputRecord, which contains two integers and a String.
+	 * Logs the String, then returns the sum of the two Integers.
+	 */
+	private static final String CHANGELOG_PATH = "C:/test/testLiqui/src/resources/changelog.sql";
+
 	@Override
-	public String handleRequest(Map<String,String> event, Context context) {
+	public String handleRequest(Map<String, String> event, Context context) {
 		try {
 			Properties props = new Properties();
-			FileInputStream fis = new FileInputStream("C:/test/testLiqui/src/resource/db.properties");
+			FileInputStream fis = new FileInputStream("C:/test/testLiqui/src/resources/db.properties");
 			props.load(fis);
 			fis.close();
-            
+
 			String url = props.getProperty("url");
 			String username = props.getProperty("username");
 			String password = props.getProperty("password");
@@ -38,21 +38,19 @@ public class testClass implements RequestHandler<Map<String,String>, String>{
 			Connection con = DriverManager.getConnection(url, username, password);
 			System.out.println(con);
 
-	        Database database = DatabaseFactory.getInstance()
-	                .findCorrectDatabaseImplementation(new JdbcConnection(con));
-	        Liquibase liquibase = new Liquibase( 
-	        		"C:/Users/HulekalS03/Downloads/eclipse-java-2022-12-R-win32-x86_64/eclipse/com.test/src/main/resources/changelog.sql", 
-	        	    new FileSystemResourceAccessor(), 
-	        	    database 
-	        	);
-	        liquibase.update( "" );
-	       
+			Database database = DatabaseFactory.getInstance()
+					.findCorrectDatabaseImplementation(new JdbcConnection(con));
+			Liquibase liquibase = new Liquibase(
+					"C:/test/testLiqui/src/resources/db.properties/changelog.sql",
+					new FileSystemResourceAccessor(),
+					database);
+			liquibase.update("");
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return null;
-		
 
 	}
 }
