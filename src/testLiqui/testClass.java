6cbb65f7998b753b957bb4078f8b7a6f311eb956
Hulekal <sachin.hulekal@hp.com>
2024-01-19 11:47:16 +0530

    uplod to aws

--- src/testLiqui/testClass.java
+++ src/testLiqui/testClass.java
@@ -1,56 +1,21 @@
 package testLiqui;
 
-import java.io.FileInputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.util.Map;
-import java.util.Properties;
-
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-import liquibase.Liquibase;
-import liquibase.database.Database;
-import liquibase.database.DatabaseFactory;
-import liquibase.database.jvm.JdbcConnection;
-import liquibase.resource.FileSystemResourceAccessor;
+import java.util.List;
 
-// Handler value: example.HandlerInteger
-public class testClass implements RequestHandler<Map<String, String>, String> {
-	/*
-	 * Takes in an InputRecord, which contains two integers and a String.
-	 * Logs the String, then returns the sum of the two Integers.
-	 */
-	private static final String CHANGELOG_PATH = "C:/test/testLiqui/src/resources/changelog.sql";
+// Handler value: example.HandlerList
+public class testClass implements RequestHandler<List<Integer>, Integer> {
 
 	@Override
-	public String handleRequest(Map<String, String> event, Context context) {
-		try {
-			Properties props = new Properties();
-			FileInputStream fis = new FileInputStream("C:/test/testLiqui/src/resources/db.properties");
-			props.load(fis);
-			fis.close();
-
-			String url = props.getProperty("url");
-			String username = props.getProperty("username");
-			String password = props.getProperty("password");
-
-			Connection con = DriverManager.getConnection(url, username, password);
-			System.out.println(con);
-
-			Database database = DatabaseFactory.getInstance()
-					.findCorrectDatabaseImplementation(new JdbcConnection(con));
-			Liquibase liquibase = new Liquibase(
-					"C:/test/testLiqui/src/resources/db.properties/changelog.sql",
-					new FileSystemResourceAccessor(),
-					database);
-			liquibase.update("");
-
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
-
+	/*
+	 * Takes a list of Integers and returns its sum.
+	 */
+	public Integer handleRequest(List<Integer> event, Context context) {
+		LambdaLogger logger = context.getLogger();
+		logger.log("EVENT TYPE: " + event.getClass().toString());
+		return event.stream().mapToInt(Integer::intValue).sum();
 	}
 }
